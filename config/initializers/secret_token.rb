f42bab9f9f13718ed8e0a63af5d040c53f4748b1
Shahin Hojabrian <shahin70000@gmail.com>
2013-03-15 10:37:48 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redben::Application.config.secret_token = '6e28092fe5be51c4097cddf0905dfb82a3f9766f32a2f19439967cd853f858ac0f3202aab3aa3ae4222a0d9ed9d63b2ab37943214d58015c2782ca7d2c85272b'
